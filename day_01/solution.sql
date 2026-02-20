7428ff8567969f68e823289e35f57d18365230e5
Kadambari Naik <89310834+kady05naik@users.noreply.github.com>
2026-02-20 07:52:47 +0100

    Update solution.sql

--- day_01/solution.sql
+++ day_01/solution.sql
@@ -1,30 +1,37 @@
 -- Return the unique countries from the films table
 SELECT 
 	DISTINCT country 
-FROM films;
+FROM 
+	films;
 
 
 -- Count the distinct countries from the films table
 SELECT 
 	COUNT(DISTINCT country) AS count_distinct_countries
-FROM films;
+FROM 
+	films;
 
 
 -- Count the Spanish-language films
 SELECT 
 	language, 
 	COUNT(language) AS count_spanish
-FROM films
-WHERE language='Spanish'
-GROUP BY language
+FROM 
+	films
+WHERE 
+	language='Spanish'
+GROUP BY 
+	language;
 
 
 -- Select the title and release_year for all German-language films released before 2000
 SELECT 
 	title, 
 	release_year
-FROM films
-WHERE language='German' 
+FROM 
+	films
+WHERE 
+	language='German' 
 	AND release_year<2000;
 
 
@@ -32,15 +39,19 @@ WHERE language='German'
 SELECT 
 	title, 
 	release_year
-FROM films
-WHERE release_year > 2000
+FROM 
+	films
+WHERE 
+	release_year > 2000
 	AND language = 'German';
 	
 	
 -- Select all records for German-language films released after 2000 and before 2010
 SELECT * 
-FROM films
-WHERE (release_year>2000 
+FROM 
+	films
+WHERE 
+	(release_year>2000 
     AND release_year<2010)
     AND language='German';
 
@@ -49,8 +60,10 @@ WHERE (release_year>2000
 SELECT 
 	title, 
 	release_year
-FROM films
-WHERE release_year = 1990 
+FROM 
+	films
+WHERE 
+	release_year = 1990 
 	OR release_year=1999;
 
 
@@ -58,8 +71,10 @@ WHERE release_year = 1990
 SELECT 
 	title, 
 	release_year
-FROM films
-WHERE release_year BETWEEN 1990 AND 2000;
+FROM 
+	films
+WHERE 
+	release_year BETWEEN 1990 AND 2000;
 
 
 -- Find the title and release_year for all films over two hours in length released in 1990 and 2000
