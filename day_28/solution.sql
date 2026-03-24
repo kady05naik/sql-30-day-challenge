7ae07755193b079f2b48d2b08c03d56e42823552
kady05naik <kady05naik@gmail.com>
2026-03-24 18:04:31 +0100

    Update day_28

--- day_28/solution.sql
+++ day_28/solution.sql
@@ -48,3 +48,51 @@ ORDER BY
 	
 	
 	
+-- find the pizza types ordered less frequently than the average for all types.
+
+SELECT pt.name,
+    pt.category,
+    SUM(od.quantity) AS total_orders
+FROM pizza_type pt
+JOIN pizzas p
+    ON pt.pizza_type_id = p.pizza_type_id
+JOIN order_details od
+    ON p.pizza_id = od.pizza_id
+GROUP BY ALL
+HAVING SUM(od.quantity) < (
+  SELECT AVG(total_quantity) 
+  FROM (
+    SELECT  SUM(od.quantity) AS total_quantity
+    FROM pizzas p
+    JOIN order_details od 
+    	ON p.pizza_id = od.pizza_id
+    GROUP BY p.pizza_id
+  ) subquery
+);
+
+
+
+-- The campaign aims to spotlight their most popular pizza based on total orders. They're also considering introducing a value meal featuring their least expensive pizza.
+-- Your task as a consulting data engineer is to identify both these pizzas.
+
+WITH  most_ordered AS (
+    SELECT pizza_id, SUM(quantity) AS total_qty 
+    FROM order_details GROUP BY pizza_id ORDER BY total_qty DESC
+    LIMIT 1
+)
+
+, cheapest_pizza AS (
+    SELECT pizza_id, price
+    FROM pizzas 
+    WHERE price = (SELECT MIN(price) FROM pizzas)
+    LIMIT 1
+)
+
+SELECT pizza_id, 'Most Ordered' AS Description, total_qty AS metric
+FROM most_ordered
+UNION ALL
+SELECT pizza_id, 'Cheapest' AS Description, price AS metric
+FROM cheapest_pizza;
+
+
+
